ec42843970d0fa94f9832da609be30550a96bfcb
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-05-16 11:17:43 +0700

    feat: sample pagination

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,10 @@
 
   let searchTerm: string = '';
   let filteredPokemons: IPokemon[] = [];
+  let currentPage = 1;
+  const itemsPerPage = 20;
+  let displayedPokemons: IPokemon[] = [];
+
   $: {
     if (searchTerm) {
       filteredPokemons = $pokemons.filter((pokemon: IPokemon) =>
@@ -13,6 +17,9 @@
     } else {
       filteredPokemons = [...$pokemons];
     }
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    displayedPokemons = filteredPokemons.slice(startIndex, endIndex);
   }
 </script>
 
@@ -47,8 +54,131 @@
   </form>
 
   <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-    {#each filteredPokemons as pokemon}
+    {#each displayedPokemons as pokemon}
       <Card name={pokemon.name} image={pokemon.image} id={pokemon.id} />
     {/each}
+  </div>
+  <div>
+    {#if !searchTerm}
+      <nav aria-label="Page navigation example">
+        <ul class="inline-flex items-center -space-x-px">
+          <li>
+            <a
+              href="/"
+              on:click|preventDefault={() => {
+                if (currentPage > 1) {
+                  currentPage--;
+                }
+              }}
+              class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span class="sr-only">Previous</span>
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  fill-rule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                /></svg
+              >
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              on:click={(e) => {
+                e.preventDefault();
+                currentPage = 1;
+              }}
+              class={currentPage !== 1
+                ? 'px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                : 'z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'}
+              >1</a
+            >
+          </li>
+          <li>
+            <a
+              href="/"
+              on:click={(e) => {
+                e.preventDefault();
+                currentPage = 2;
+              }}
+              class={currentPage !== 2
+                ? 'px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                : 'z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'}
+              >2</a
+            >
+          </li>
+          <li>
+            <a
+              href="/"
+              on:click={(e) => {
+                e.preventDefault();
+                currentPage = 3;
+              }}
+              class={currentPage !== 3
+                ? 'px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                : 'z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'}
+              >3</a
+            >
+          </li>
+          <li>
+            <a
+              href="/"
+              on:click={(e) => {
+                e.preventDefault();
+                currentPage = 4;
+              }}
+              class={currentPage !== 4
+                ? 'px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                : 'z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'}
+              >4</a
+            >
+          </li>
+          <li>
+            <a
+              href="/"
+              on:click={(e) => {
+                e.preventDefault();
+                currentPage = 5;
+              }}
+              class={currentPage !== 5
+                ? 'px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                : 'z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'}
+              >5</a
+            >
+          </li>
+          <li>
+            <a
+              href="/"
+              on:click|preventDefault={() => {
+                if (currentPage < 100) {
+                  currentPage++;
+                }
+              }}
+              class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span class="sr-only">Next</span>
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                /></svg
+              >
+            </a>
+          </li>
+        </ul>
+      </nav>
+    {/if}
   </div>
 </div>
